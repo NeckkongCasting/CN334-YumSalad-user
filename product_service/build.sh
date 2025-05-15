8a24c853029914f33b593aa93e60a8b60a2e3723
Lonelyboys <prompiriya1421@gmail.com>
2025-05-15 21:50:28 +0700

    build

--- product_service/build.sh
+++ product_service/build.sh
@@ -8,5 +8,15 @@ pip install -r requirements.txt
 # Run migrations
 python manage.py migrate
 
+# Create superuser automatically
+python manage.py shell -c "
+from django.contrib.auth import get_user_model;
+User = get_user_model();
+if not User.objects.filter(username='admin').exists():
+    User.objects.create_superuser('admin', 
+                                'admin@example.com', 
+                                'Admin123!')
+"
+
 # Collect static files
 python manage.py collectstatic --no-input 
