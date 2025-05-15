580131ab5f9b23051c2b862b827e1dc0ba958c90
Lonelyboys <prompiriya1421@gmail.com>
2025-05-15 19:49:42 +0700

    new setting

--- product_service/product_service/settings.py
+++ product_service/product_service/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'product_management',
+    'user_management',
     'rest_framework',
     'corsheaders',
 ]
